e75a1aed722b1fd5fa3c2f90beb7ecfaebe62c42
John Benny Fabonan <133860457+JohnBennyFabonan@users.noreply.github.com>
2026-01-23 00:18:55 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
